263a5dceb24dbcbf860fcb9c1ea2d2b82379b914
2danbi <2daaanbi@gmail.com>
2023-01-09 16:27:07 +0900

    add naudio

--- OpenCVForm/Form1.Designer.cs
+++ OpenCVForm/Form1.Designer.cs
@@ -32,10 +32,12 @@
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.btn_start = new System.Windows.Forms.Button();
             this.panel2 = new System.Windows.Forms.Panel();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.panel1 = new System.Windows.Forms.Panel();
             this.lb_serverMsg = new System.Windows.Forms.Label();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.panel2.SuspendLayout();
             this.panel1.SuspendLayout();
@@ -45,13 +47,13 @@
             // 
             this.pictureBox1.Location = new System.Drawing.Point(3, 3);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(640, 480);
+            this.pictureBox1.Size = new System.Drawing.Size(640, 375);
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
             // 
             // btn_start
             // 
-            this.btn_start.Location = new System.Drawing.Point(661, 46);
+            this.btn_start.Location = new System.Drawing.Point(661, 14);
             this.btn_start.Name = "btn_start";
             this.btn_start.Size = new System.Drawing.Size(166, 71);
             this.btn_start.TabIndex = 1;
@@ -61,6 +63,8 @@
             // 
             // panel2
             // 
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Controls.Add(this.progressBar1);
             this.panel2.Controls.Add(this.textBox1);
             this.panel2.Controls.Add(this.pictureBox1);
             this.panel2.Controls.Add(this.btn_start);
@@ -69,21 +73,28 @@
             this.panel2.Size = new System.Drawing.Size(844, 492);
             this.panel2.TabIndex = 3;
             // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(3, 395);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(640, 83);
+            this.progressBar1.TabIndex = 5;
+            // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(661, 174);
+            this.textBox1.Location = new System.Drawing.Point(661, 101);
             this.textBox1.Multiline = true;
             this.textBox1.Name = "textBox1";
             this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
-            this.textBox1.Size = new System.Drawing.Size(166, 309);
+            this.textBox1.Size = new System.Drawing.Size(166, 277);
             this.textBox1.TabIndex = 2;
             // 
             // panel1
             // 
             this.panel1.Controls.Add(this.lb_serverMsg);
-            this.panel1.Location = new System.Drawing.Point(12, 9);
+            this.panel1.Location = new System.Drawing.Point(35, 510);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(844, 492);
+            this.panel1.Size = new System.Drawing.Size(844, 453);
             this.panel1.TabIndex = 4;
             // 
             // lb_serverMsg
@@ -100,13 +111,22 @@
             this.timer1.Interval = 33;
             this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(700, 426);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(39, 15);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "label1";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(865, 516);
-            this.Controls.Add(this.panel1);
             this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
@@ -126,5 +146,7 @@
         private Label lb_serverMsg;
         private System.Windows.Forms.Timer timer1;
         private TextBox textBox1;
+        private ProgressBar progressBar1;
+        private Label label1;
     }
 }
--- OpenCVForm/Form1.cs
+++ OpenCVForm/Form1.cs
@@ -1,3 +1,5 @@
+using Microsoft.VisualBasic.Devices;
+using NAudio.CoreAudioApi;
 using OpenCvSharp;
 
 namespace OpenCVForm
@@ -8,10 +10,23 @@ namespace OpenCVForm
         Client client = new Client();
         Mat img = new Mat();
         bool isDriving = false;
+        int decibel_count = 0;
 
         public Form1()
         {
             InitializeComponent();
+
+
+            var waveIn = new NAudio.Wave.WaveInEvent
+            {
+                DeviceNumber = 0, // indicates whick microphone to use
+                WaveFormat = new NAudio.Wave.WaveFormat(rate: 44100, bits: 16, channels: 1),
+                BufferMilliseconds = 20
+            };
+
+            waveIn.DataAvailable += WaveIn_DataAvailable;
+            waveIn.StartRecording();
+
         }
 
         private void Form1_Load(object sender, EventArgs e)
@@ -21,7 +36,7 @@ namespace OpenCVForm
             client.DataResponsed += client_DataResponsed;
 
             // 클라이언트와 서버의 연결 시작
-            client.Connect();
+            //client.Connect();
             timer1.Start();
         }
 
@@ -91,6 +106,30 @@ namespace OpenCVForm
                 isDriving = false;
                 btn_start.Text = "Start";
             }
+        }
+        private void label1_click(object sender, EventArgs e)
+        {
+
+        }
+
+
+        private void WaveIn_DataAvailable(object sender, NAudio.Wave.WaveInEventArgs e)
+        {
+            // copy buffer into an array of integers
+            Int16[] values = new Int16[e.Buffer.Length / 2];
+            Buffer.BlockCopy(e.Buffer, 0, values, 0, e.Buffer.Length);
+
+            // determine the highest value as a fraction of the maximum possible value
+            float fraction = (float)values.Max() / 32768;
+
+            int decibel = (int)(fraction * 100);
+
+            // print a level meter using the console
+            progressBar1.Value = decibel;
+            if (decibel == 99)
+                decibel_count += 1;
+            label1.Text = decibel_count.ToString();
+          
         }
     }
 }
